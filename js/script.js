9747f57706e75827de241bc6d10d8e4a16c3a313
FernandoVerderame <fernandoverderame96@hotmail.com>
2023-12-15 16:36:33 +0100

    name request

--- js/script.js
+++ js/script.js
@@ -5,3 +5,7 @@ console.log(paragraph);
 // ! 2 Costruiamo il messaggio
 let message = 'La tua password è: ';
 console.log(message);
+
+// ! 3 Chiediamo il nome all'utente
+const userName = prompt('Qual è il tuo nome?');
+console.log(userName);
