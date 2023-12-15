9962d5e43f3b902fd896c018ed7d257a1b85dbe0
FernandoVerderame <fernandoverderame96@hotmail.com>
2023-12-15 16:32:00 +0100

    built message

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
 // ! 1 Prendo l'elemento dal DOM (la p)
 const paragraph = document.getElementById('result');
 console.log(paragraph);
+
+// ! 2 Costruiamo il messaggio
+let message = 'La tua password è: ';
+console.log(message);
